4e0f3d86589fd6507d1394001214aa2750c2ae55
Kachnov <m79054050200@gmail.com>
2019-02-28 11:31:38 +0300

    added parse json

--- graph.js
+++ graph.js
@@ -1,21 +1,70 @@
-var trace1 = {
-  x: [1, 2, 3],
-  y: [4, 5, 6],
-  type: 'scatter'
-};
+var parseData = JSON.parse('{"data":[{"time":0,"height":[0,100,200,300,400,500,600,700,800,900,1000],"temperature":[2.1,2.5,1.7,1.4,1,1.4,0.9,0.6,0.3,0,0.4]},{"time":30,"height":[0,100,200,300,400,500,600,700,800,900,1000],"temperature":[2.5,2.1,2.7,2.3,2,1.8,2.2,1.9,1.4,1,0.6]},{"time":60,"height":[0,100,200,300,400,500,600,700,800,900,1000],"temperature":[4.1,4.5,3.7,3.4,3,3.4,2.9,2.6,2.3,2,2.4]}]}');
 
-var trace2 = {
-  x: [20, 30, 40],
-  y: [50, 60, 70],
-  type: 'scatter',
-  xaxis: 'x2',
-  yaxis: 'y',
-};
+function createGraph(data, name, xaxis) {
+  var x = [];
+  var y = [];
+  for (var i = 0; i < data.height.length; i++) {
+    y.push(data.height[i]);
+    x.push(data.temperature[i]);
+  }
+  return {
+    x: x,
+    y: y,
+    type: 'scatter',
+    xaxis: xaxis,
+    yaxis: 'y',
+    name: name
+  }
+}
 
-var data = [trace1, trace2];
+var time0 = createGraph(parseData.data[0], 'Время - 0', 'x');
+var time1 = createGraph(parseData.data[1], 'Время - 30', 'x2');
+var time2 = createGraph(parseData.data[2], 'Время - 60', 'x3');
+
+// var time0 = {
+//   x: [],
+//   y: [],
+//   type: 'scatter',
+//   name: 'Время - 0'
+// };
+//
+// for (var i = 0; i < parseData.data[0].height.length; i++) {
+//   time0.y.push(parseData.data[0].height[i]);
+//   time0.x.push(parseData.data[0].temperature[i]);
+// }
+//
+// var time1 = {
+//   x: [],
+//   y: [],
+//   type: 'scatter',
+//   xaxis: 'x2',
+//   yaxis: 'y',
+//   name: 'Время - 30'
+// };
+//
+// for (var i = 0; i < parseData.data[1].height.length; i++) {
+//   time1.y.push(parseData.data[1].height[i]);
+//   time1.x.push(parseData.data[1].temperature[i]);
+// }
+//
+// var time2 = {
+//   x: [],
+//   y: [],
+//   type: 'scatter',
+//   xaxis: 'x3',
+//   yaxis: 'y',
+//   name: 'Время - 60'
+// };
+//
+// for (var i = 0; i < parseData.data[2].height.length; i++) {
+//   time2.y.push(parseData.data[2].height[i]);
+//   time2.x.push(parseData.data[2].temperature[i]);
+// }
+
+var data = [time0, time1, time2];
 
 var layout = {
-  grid: {subplots:[['xy', 'x2y']]},
+  grid: {subplots:[['xy', 'x2y', 'x3y']]},
 };
 
 var gd = document.getElementById('myDiv')
